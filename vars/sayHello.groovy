684fcf052a079fbc4a19c9cbfc77fcffa3f2a51f
Tejas Girase <tejas.girase@infostretch.com>
2021-02-04 16:33:34 +0530

    added groovy stage

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -4,15 +4,12 @@ def call(String name = 'human') {
     // Any valid steps can be called from this code, just like in other
     // Scripted Pipeline
     echo "Hello, ${name}."
-    stage ('Call eslint check') {
-        steps {
-            script {
-                sh '''
-                rm -rf package-lock.json
-                npm install
-                npm run build
+
+    script {
+        sh '''
+        rm -rf package-lock.json
+        npm install
+        npm run build
                 '''
-            }
-        }
     }
 }
