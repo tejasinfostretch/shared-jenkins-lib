15f43e6745538e047d0e963b0f2729293fe6997d
Tejas Girase <tejas.girase@infostretch.com>
2021-02-06 01:05:39 +0530

    fix error catch

--- vars/dodTask.groovy
+++ vars/dodTask.groovy
@@ -1,10 +1,15 @@
 def call() {
     copyJsFileIntoWorkspace()
+    try {
 
-    sh '''
-        rm -rf package-lock.json
-        npm install
-        npm install shelljs
-        node scanFile.js
-                '''
+        sh '''
+            rm -rf package-lock.json
+            npm install
+            npm install shelljs
+            node scanFile.js
+                    '''
+    } catch(e) {
+        printl('ERRORRRR.......................')
+        printl(e)
+    }
 }
