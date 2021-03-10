84c08e1435b9a355a14331da7d44da37e915cf5a
Tejas Girase <tejas.girase@infostretch.com>
2021-03-10 11:27:35 +0530

    added query paramater

--- vars/dodTask.groovy
+++ vars/dodTask.groovy
@@ -7,28 +7,31 @@ def call(init = true) {
             npmInit = 'npm i'
         }
 
-        def test = 'this is string'
-
-        def someMap = [
-            'key1': 'value',
-            'key2': 142.1,
-            'key3': test
-        ]
-
-        def json = new groovy.json.JsonBuilder()
-
-        json rootKey: someMap
-
-        def data = json.toString()
-
         sh '''
             cd dodCheck
             echo '''+npmInit+'''
 
-            node dod/qTest.dod.js ${data}
+            node dod/qTest.dod.js ${getData()}
         '''
     } catch (e) {
         println('ERRORRRR.......................')
         println(e)
     }
+}
+
+def getData() {
+    def test = 'this is string'
+
+    def someMap = [
+            'key1': 'value',
+            'key2': 142.1,
+            'key3': test
+        ]
+
+    def json = new groovy.json.JsonBuilder()
+
+    json rootKey: someMap
+
+    def data = json.toString()
+    return data
 }
